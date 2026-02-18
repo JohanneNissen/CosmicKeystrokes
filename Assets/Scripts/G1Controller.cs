7efbf54c1328c4c2d67373aa2c04daf6a795e6fc
Nr1dad <emilh23@student.sdu.dk>
2026-02-18 07:23:44 +0100

    Added models, Textures and scene for G1

---
+++ Assets/Scripts/G1Controller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class G1Controller : MonoBehaviour
+{
+
+    public float speed = 5f; 
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/ScrollUV.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+
+    public float parralax = 2f;
+ 
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.y = transform.position.z / transform.localScale.z / parralax;
+
+        mat.mainTextureOffset = offset;
+
+
+    }
+}
