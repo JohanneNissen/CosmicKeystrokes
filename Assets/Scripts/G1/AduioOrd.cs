b94af78c8a60dec6b779939c21190961e5f52952
Nr1dad <emilh23@student.sdu.dk>
2026-02-28 15:04:59 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Scripts/G1/AduioOrd.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AduioOrd : MonoBehaviour
+{
+
+    public AudioClip[] audioClips;
+    
+
+
+    private void Awake()
+    {
+       
+        
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/G1/G1ManagerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using UnityEngine;
+
+public class G1ManagerScript : MonoBehaviour
+{
+    public AudioSource source;
+    public HashTest ord;
+    public AudioClip næsteOrd;
+
+
+    private void Awake()
+    {
+        ord = gameObject.GetComponent<HashTest>();
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Start()
+    {
+        StartCoroutine(PlayNextWord());
+    }
+
+
+    IEnumerator PlayNextWord()
+    {
+        source.clip = næsteOrd;
+        source.Play();
+        yield return new WaitForSeconds(.5f);
+
+        source.clip = ord.GetAudio(GenerateWord());
+
+        source.Play();
+    }
+
+   
+
+    void ReplayCurrentWord()
+    {
+
+
+    }
+    
+
+    string GenerateWord()
+    {
+
+    }
+
+
+}
