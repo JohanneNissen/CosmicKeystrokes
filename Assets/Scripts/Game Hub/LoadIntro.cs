207caf09b3c841cafb4706e212d29a4f564cdbd0
Nr1Dad <144018778+Nr1Dad@users.noreply.github.com>
2026-04-13 08:04:37 +0200

    Hub: Add loaders for intro + G3

---
+++ Assets/Scripts/Game Hub/LoadIntro.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadIntro : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(5);
+        }
+
+
+    }
+
+}
---
+++ Assets/Scripts/Game Hub/Loadgame3.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadgame3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadSceneAsync(4);
+        }
+
+
+    }
+}
