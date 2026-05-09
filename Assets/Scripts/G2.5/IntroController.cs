c1b3ada51ec3e964837199e0170c84f70af50231
johanne <johanne@ornsovej.dk>
2026-05-09 17:47:17 +0200

    Added descriptions to game 2.5
    Added text to introduction description for game 2.5

--- Assets/Scripts/G2.5/IntroController.cs
+++ Assets/Scripts/G2.5/IntroController.cs
@@ -9,9 +9,10 @@ public class IntroController : MonoBehaviour
 
     int introCount = 1;
 
-    string intro1 = "Velkommen til Strøm Dirigering.";
-    string intro2 = "intro 2";
-    string intro3 = "intro 3";
+    string intro1 = "Velkommen til Strøm Dirigering. Rumstationen har haft strømafbrydelse, og vi har brug for din hjælp til at tænde strømmen igen. For at gøre det skal du dirigerer elektrisiteten hen til det grønne felt.";
+    string intro2 = "For at styre strømmen, skal du skrive ordene som passer til den retning, du vil flytte strømmen i. Tryk MELLEMRUM når du har skrevet ordet, for at flytte strømmen.";
+    string intro3 = "Når du flytter strømmen fortsætter den i den retning du har valgt ind til den rammer noget, som stopper den. De røde bokse er vægge, som stopper strømmen foran sig. De orange felter stopper strømmen ovenpå sig.";
+    string intro4 = "Skriv ordene for at flytte strømmen og før den hen til det grønne felt. Tryk ENTER for at starte!";
 
     public bool gameRunning = false;
     void Start()
@@ -38,6 +39,9 @@ public class IntroController : MonoBehaviour
                         introtext.text = intro3;
                         break;
                     case 4:
+                        introtext.text = intro4;
+                        break;
+                    case 5:
                         robot.gameObject.SetActive(false);
                         gameRunning = true;
                         break;
