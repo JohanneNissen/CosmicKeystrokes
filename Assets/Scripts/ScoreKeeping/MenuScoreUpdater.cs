fb1be05c18e14b21dfb82483f37587b8ecf1ea11
johanne <johanne@ornsovej.dk>
2026-06-21 17:01:24 +0200

    Added end screen to game 2
    Added an end screen after reaching the goal in game 2

--- Assets/Scripts/ScoreKeeping/MenuScoreUpdater.cs
+++ Assets/Scripts/ScoreKeeping/MenuScoreUpdater.cs
@@ -4,7 +4,7 @@ using TMPro;
 public class MenuScoreUpdater : MonoBehaviour
 {
     [SerializeField] private TMP_Text game1ScoreTx;
-    [SerializeField] private TMP_Text game2ScoreTx;
+    //[SerializeField] private TMP_Text game2ScoreTx;
     [SerializeField] private TMP_Text game3ScoreTx;
 
     void Start()
@@ -20,7 +20,7 @@ public class MenuScoreUpdater : MonoBehaviour
     void UpdateScores()
     {
         game1ScoreTx.text = $"<color=#6EC6FF>Rum Ræs: </color>{HighScoreTracker.HSTInstance.Minigame1.Highscore}";
-        game2ScoreTx.text = $"<color=#6EC6FF>Strøm Dirigering: </color>{HighScoreTracker.HSTInstance.Minigame2.Highscore}";
+        //game2ScoreTx.text = $"<color=#6EC6FF>Strøm Dirigering: </color>{HighScoreTracker.HSTInstance.Minigame2.Highscore}";
         game3ScoreTx.text = $"<color=#6EC6FF>Kantine Kaos:</color> \n" +
                             $"<color=#7DFFB3>Præsition: </color>{HighScoreTracker.HSTInstance.Minigame3Acc.Highscore}%\n" +
                             $"<color=#7DFFB3>Antal Ord: </color>{HighScoreTracker.HSTInstance.Minigame3Words.Highscore}";
--- Assets/Scripts/StroemDirigering/IntroController.cs
+++ Assets/Scripts/StroemDirigering/IntroController.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class IntroController : MonoBehaviour
@@ -13,9 +14,10 @@ public class IntroController : MonoBehaviour
     string intro2 = "For at styre strømmen, skal du skrive ordene som passer til den retning, du vil flytte strømmen i. Tryk MELLEMRUM når du har skrevet ordet, for at flytte strømmen.";
     string intro3 = "Når du flytter strømmen fortsætter den i den retning du har valgt ind til den rammer noget, som stopper den. De røde bokse er vægge, som stopper strømmen foran sig. De orange felter stopper strømmen ovenpå sig.";
     string intro4 = "Skriv ordene for at flytte strømmen og før den hen til det grønne felt. Tryk ENTER for at starte!";
-    string intro6 = "Godt klaret! Strømmen er tilbage i rumstationen.";
+    string intro6 = "Godt klaret! Strømmen er tilbage i rumstationen. Tryk på ENTER for at gå tilbage";
 
     public bool gameRunning = false;
+    public bool goalReached = false;
     void Start()
     {
         introtext.text = intro1;
@@ -25,7 +27,7 @@ public class IntroController : MonoBehaviour
     {
         if (!gameRunning)
         {
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (Input.GetKeyDown(KeyCode.Return) && !goalReached)
             {
                 introCount++;
                 switch (introCount)
@@ -46,13 +48,22 @@ public class IntroController : MonoBehaviour
                         robot.gameObject.SetActive(false);
                         gameRunning = true;
                         break;
-                    case 6:
-                        robot.gameObject.SetActive(true);
-                        gameRunning = false;
-                        introtext.text = intro6;
-                        break;
                 }
             }
+
+            if (goalReached == true)
+            {
+                robot.gameObject.SetActive(true);
+                introtext.text = intro6;
+
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    introCount = 1;
+                    SceneManager.LoadSceneAsync(1);
+                }
+                
+            }
+
         }
     }
 }
